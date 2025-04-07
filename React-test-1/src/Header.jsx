4eebe6f789a66c047e659f70efda8cfda806d16f
Jenny2807 <jenny.okafor@gmx.de>
2025-04-07 17:42:27 +0200

    initial Commit for React project

---
+++ React-test-1/src/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'; 
+
+function Header() {
+    return (
+      <header>
+        <h1>Das ist ein Test</h1>
+        <nav>
+          <ul>
+            <li><a href ="https://www.google.com" >Home</a></li>
+            <li><a href ="https://www.google.com" >About</a></li>
+            <li><a href ="https://www.google.com" >Services</a></li>
+            <li><a href ="https://www.google.com" >Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Header; 
